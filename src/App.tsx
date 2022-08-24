ee67eac8b0a056f616d37c0d9de6d3e8ce34105c
Colin Spence <udappia@gmail.com>
2022-08-24 17:13:43 -0600

    updates to navbar/layout

--- src/App.tsx
+++ src/App.tsx
@@ -3,15 +3,19 @@ import { Route, Routes } from "react-router-dom";
 import { Navbar } from "./layout/Navbar";
 import { Footer } from "./layout/Footer";
 import { HomeView } from "./views/home/Home";
+import { JoinUsView } from "./views/join-us/JoinUs";
+import { HireUsView } from "./views/hire-us/HireUs";
 
 const App: React.FC = () => {
   return (
-    <Navbar>
-      <Routes>
-        <Route path='/' element={<HomeView />} />
-      </Routes>
+    <>
+      <Navbar>
+        <Routes>
+          <Route path='/' element={<HomeView />} />
+        </Routes>
+      </Navbar>
       <Footer />
-    </Navbar>
+    </>
   );
 };
 
--- src/layout/Navbar.tsx
+++ src/layout/Navbar.tsx
@@ -1,11 +1,12 @@
 import React from "react";
 import { Header } from "../commons/header/Header";
+import { Box } from "@mui/material";
 
 export const Navbar: React.FC = ({ children }) => {
   return (
-    <>
+    <Box sx={{minHeight: "100vh"}}>
       <Header />
       {children}
-    </>
+    </Box>
   );
 };
