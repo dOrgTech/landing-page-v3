de5c184f29a4aed6495d87bc68a1fffc8ad0cebd
Colin Spence <udappia@gmail.com>
2022-08-26 13:29:28 -0600

    Update Button.tsx

--- src/commons/button/Button.tsx
+++ src/commons/button/Button.tsx
@@ -1,23 +1,25 @@
 import { Button as MuiButton, ButtonProps, styled } from "@mui/material";
 import React from "react";
+import { colors } from "../../theme";
 
 const OutlinedButton = styled(MuiButton)({
+  backgroundColor: colors.black,
   width: "100%",
   "&:hover": {
-    border: "4px solid #E5E5E5",
-    backgroundColor: "#222222",
-    boxShadow: "8px 7px 0 0 #73F260",
+    border: `4px solid ${colors.grays[100]}`,
+    backgroundColor: colors.grays[800],
+    boxShadow: `8px 7px 0 0 ${colors.green}`,
   },
 });
 
 const ContainedButton = styled(MuiButton)({
   width: "100%",
-  background: "#E5E5E5",
-  color: "#000",
+  background: colors.grays[100],
+  color: colors.black,
   "&:hover": {
-    backgroundColor: "#222222",
-    color: "#E5E5E5",
-    boxShadow: "8px 7px 0 0 #73F260",
+    backgroundColor: colors.grays[800],
+    color: colors.grays[100],
+    boxShadow: `8px 7px 0 0 ${colors.green}`,
   },
 });
 
