2c203e2f92a3c704fac6730c2094d0a784936862
Colin Spence <udappia@gmail.com>
2022-10-03 11:48:02 -0600

    Adds 7 more community posts

--- src/commons/twitter/TwitterCard.tsx
+++ src/commons/twitter/TwitterCard.tsx
@@ -1,8 +1,8 @@
 import React, { MouseEvent } from "react";
 import { Box, Link, Stack, Typography } from "@mui/material";
-import { colors } from "../../theme"
+import { colors } from "../../theme";
 import TwitterIcon from "../../assets/imgs/twitter.svg";
-import { TwitterAccountProps } from "../../constants/twitterAccounts"
+import { TwitterAccountProps } from "../../constants/twitterAccounts";
 
 export interface TwitterCardProps {
   account: TwitterAccountProps;
@@ -13,17 +13,30 @@ export interface TwitterCardProps {
   clickable?: boolean;
 }
 
-export const TwitterCard: React.FC<TwitterCardProps> = ({ account, externalLink, text, image, color, clickable }) => {
+export const TwitterCard: React.FC<TwitterCardProps> = ({
+  account,
+  externalLink,
+  text,
+  image,
+  color,
+  clickable,
+}) => {
   const { avatar, name, username } = account;
 
   const handleClick = (e: MouseEvent) => {
-    if(!clickable) {
+    if (!clickable) {
       e.preventDefault();
     }
-  }
+  };
 
   return (
-    <Link underline="none" href={externalLink} target="_blank" draggable={false} onClick={(e) => handleClick(e)}>
+    <Link
+      underline="none"
+      href={externalLink}
+      target="_blank"
+      draggable={false}
+      onClick={(e) => handleClick(e)}
+    >
       <Stack
         direction="column"
         spacing={2}
@@ -34,41 +47,59 @@ export const TwitterCard: React.FC<TwitterCardProps> = ({ account, externalLink,
           color: colors.white,
           cursor: "pointer",
           p: 2,
+          textAlign: "left",
           transition: "backgroundColor 0.25s ease-in-out",
           "&:hover": {
-            bgcolor: colors.grays[800]
-          }
+            bgcolor: colors.grays[800],
+          },
         }}
       >
         <Stack direction="row" justifyContent="space-between">
           <Stack direction="row" spacing={1} alignItems="center">
-            <Box sx={{
-              width: 32,
-              height: 32,
-              borderRadius: 999,
-              overflow: "hidden"
-            }}>
-              <img src={avatar} alt={username}/>
+            <Box
+              sx={{
+                width: 32,
+                height: 32,
+                borderRadius: 999,
+                overflow: "hidden",
+              }}
+            >
+              <img src={avatar} alt={username} />
             </Box>
             <Box>
-              <Typography variant="body2" lineHeight={1.25} fontWeight={800}>{name}</Typography>
-              <Typography variant="body2" lineHeight={1.25} fontWeight={100} color={colors.grays[500]}>{username}</Typography>
+              <Typography variant="body2" lineHeight={1.25} fontWeight={800}>
+                {name}
+              </Typography>
+              <Typography
+                variant="body2"
+                lineHeight={1.25}
+                fontWeight={100}
+                color={colors.grays[500]}
+              >
+                {username}
+              </Typography>
             </Box>
           </Stack>
-          <Box sx={{
-            opacity: 0.8,
-            transition: "opacity 0.25s ease-in-out",
-            width: 20,
-            "&:hover": {
-              opacity: 0.6
-            }
-          }}>
+          <Box
+            sx={{
+              opacity: 0.8,
+              transition: "opacity 0.25s ease-in-out",
+              width: 20,
+              "&:hover": {
+                opacity: 0.6,
+              },
+            }}
+          >
             <img src={TwitterIcon} alt="Twitter" />
           </Box>
         </Stack>
         {text}
         {image && (
-          <img src={image} alt="" style={{borderRadius: 8, pointerEvents: "none"}}/>
+          <img
+            src={image}
+            alt=""
+            style={{ borderRadius: 8, pointerEvents: "none" }}
+          />
         )}
       </Stack>
     </Link>
--- src/views/home/components/CommunitySection/Tweets/CommunityContent.tsx
+++ src/views/home/components/CommunitySection/Tweets/CommunityContent.tsx
@@ -1,77 +1,244 @@
 import React from "react";
 import { styled, Typography } from "@mui/material";
-import { PostLink } from "../../../../../commons/twitter/PostLink"
+import { PostLink } from "../../../../../commons/twitter/PostLink";
 
 const ContentTypography = styled(Typography)({
   lineHeight: 1.25,
   fontWeight: 500,
-})
+});
 
 const MagentaText = () => {
   return (
     <ContentTypography variant="subtitle2">
-      <span role="img" aria-label="speech balloon">💬</span> &quot;The
-      most striking thing about dOrg is everyone here are incredible people. 
-      Brilliant and human-centered in why we are doing what we are doing.&quot;
-      Learn more about dOrg&apos;s <PostLink>@magentaceiba</PostLink> and her
-      thoughts on the DAO here: <PostLink>https://bit.ly/3j4NKhW</PostLink>
+      <span role="img" aria-label="speech balloon">
+        💬
+      </span>{" "}
+      &ldquo;The most striking thing about dOrg is everyone here are incredible
+      people. Brilliant and human-centered in why we are doing what we are
+      doing.&rdquo; Learn more about dOrg&apos;s{" "}
+      <PostLink>@magentaceiba</PostLink> and her thoughts on the DAO here:{" "}
+      <PostLink>https://bit.ly/3j4NKhW</PostLink>
     </ContentTypography>
-  )
-}
+  );
+};
 
 const LinusText = () => {
   return (
     <ContentTypography variant="subtitle2">
-      Merry Christmas to me <span role="img" aria-label="smiley glasses">😎</span> <PostLink>@dOrg_tech</PostLink> <PostLink>@TheMetaFactory</PostLink>
+      Merry Christmas to me{" "}
+      <span role="img" aria-label="smiley glasses">
+        😎
+      </span>{" "}
+      <PostLink>@dOrg_tech</PostLink> <PostLink>@TheMetaFactory</PostLink>
     </ContentTypography>
-  )
-}
+  );
+};
 
 const PhilText = () => {
   return (
     <ContentTypography variant="subtitle2">
-      Nous sommes dOrg <span role="img" aria-label="heart">❤️</span>
-      <br/>
-      <br/>
-      With <PostLink>@dOrgJelli</PostLink> <PostLink>@MrsBadgerface</PostLink> <PostLink>@DaoAdvocate</PostLink> <PostLink>@ronboger</PostLink>
-       - <PostLink>@dOrg_tech</PostLink> at <PostLink>@EthCC</PostLink> <PostLink>@TheDAOist_</PostLink>
+      Nous sommes dOrg{" "}
+      <span role="img" aria-label="heart">
+        ❤️
+      </span>
+      <br />
+      <br />
+      With <PostLink>@dOrgJelli</PostLink> <PostLink>@MrsBadgerface</PostLink>{" "}
+      <PostLink>@DaoAdvocate</PostLink> <PostLink>@ronboger</PostLink>-{" "}
+      <PostLink>@dOrg_tech</PostLink> at <PostLink>@EthCC</PostLink>{" "}
+      <PostLink>@TheDAOist_</PostLink>
     </ContentTypography>
-  )
-}
+  );
+};
 
 const NewsletterText = () => {
   return (
     <ContentTypography variant="subtitle2">
-      Check out the latest installment of our <span role="img" aria-label="spark">✨</span>occasional<span role="img" aria-label="spark">✨</span> newsletter, where we discuss:
-      <br/>
-      <span role="img" aria-label="bee">🐝</span> Swarm Intelligence
-      <br/>
-      <span role="img" aria-label="running shoe">👟</span> Automata Sneakers
-      <br/>
-      <span role="img" aria-label="see-no-evil monkey">🙈</span> The Opposite of FOMO
-      <br/>
+      Check out the latest installment of our{" "}
+      <span role="img" aria-label="spark">
+        ✨
+      </span>
+      occasional
+      <span role="img" aria-label="spark">
+        ✨
+      </span>{" "}
+      newsletter, where we discuss:
+      <br />
+      <span role="img" aria-label="bee">
+        🐝
+      </span>{" "}
+      Swarm Intelligence
+      <br />
+      <span role="img" aria-label="running shoe">
+        👟
+      </span>{" "}
+      Automata Sneakers
+      <br />
+      <span role="img" aria-label="see-no-evil monkey">
+        🙈
+      </span>{" "}
+      The Opposite of FOMO
+      <br />
       ...and a lot more ;)
-      <br/>
+      <br />
       <PostLink>https://bit.ly/3BUBK8F</PostLink>
     </ContentTypography>
-  )
-}
+  );
+};
 
 const GivethText = () => {
   return (
     <ContentTypography variant="subtitle2">
-      Awwww. Feeling the love remotely joining <PostLink>@dOrg_tech</PostLink>&apos;s unconference day in the 
-      <PostLink>@Givethio</PostLink> house in Spain where one of the lovely <PostLink>@commonsstack</PostLink>
-      and <PostLink>@tecmns</PostLink> folk that I know is hosting and we had one of those &quot;hello!&quot;
-      happy moments <span role="img" aria-label="smile with heart-eyes">😍</span>
+      Awwww. Feeling the love remotely joining <PostLink>@dOrg_tech</PostLink>
+      &apos;s unconference day in the <PostLink>@Givethio</PostLink> house in
+      Spain where one of the lovely <PostLink>@commonsstack</PostLink> and{" "}
+      <PostLink>@tecmns</PostLink> folk that I know is hosting and we had one of
+      those &quot;hello!&quot; happy moments{" "}
+      <span role="img" aria-label="smile with heart-eyes">
+        😍
+      </span>
     </ContentTypography>
-  )
-}
+  );
+};
+
+const UnconferenceText = () => {
+  return (
+    <ContentTypography variant="subtitle2">
+      Our unconference day was a success!{" "}
+      <span role="img" aria-label="raised hands">
+        🙌🏻
+      </span>
+      <br />
+      <br />
+      We shared new ideas, projects and achievements among dOrg members over
+      drinks and snacks.
+      <br />
+      <br />
+      It was an amazing session!{" "}
+      <span role="img" aria-label="rocket">
+        🚀
+      </span>
+    </ContentTypography>
+  );
+};
+
+const GranCanariaText = () => {
+  return (
+    <ContentTypography variant="subtitle2">
+      We had a blast at our presentation in Gran Canaria!{` `}
+      <span role="img" aria-label="desert-island">
+        🏝
+      </span>
+      <br />
+      <br />
+      We were invited by Thriving Nomads and it was quite an experience, check
+      out the attached photos to feel the vibe of the event!{` `}
+      <span role="img" aria-label="lightning">
+        ⚡️
+      </span>
+    </ContentTypography>
+  );
+};
+
+const EthBerlinText = () => {
+  return (
+    <ContentTypography variant="subtitle2">
+      Thanks to <PostLink>@ETHBerlin</PostLink> for an amazing weekend!
+      <br />
+      <br />
+      Inspiring talks, great projects and talented people{` `}
+      <span role="img" aria-label="comet">
+        ☄️
+      </span>
+      <br />
+      <br />
+      Check out some evidence of this wonderful experience{` `}
+      <span role="img" aria-label="backhand index finger pointing down">
+        👇
+      </span>
+    </ContentTypography>
+  );
+};
+
+const MetaFamText = () => {
+  return (
+    <ContentTypography variant="subtitle2">
+      Check out our talk at Metafest2 where{` `}
+      <PostLink>@namestyd</PostLink> walks through how dOrg works why builders
+      join.
+      <br />
+      <br />
+      Thanks for the invitation{` `}
+      <PostLink>@MetaFam</PostLink>!{` `}
+      <span role="img" aria-label="octopus">
+        🐙
+      </span>
+    </ContentTypography>
+  );
+};
+
+const KlerosText = () => {
+  return (
+    <ContentTypography variant="subtitle2">
+      Thanks to <PostLink>@federicoast</PostLink> and{" "}
+      <PostLink>@JayBuidl</PostLink>
+      {` `}
+      for speaking at yesterday&apos;s All Hands Meeting about the latest doings
+      of <PostLink>@Kleros_io</PostLink>
+      <br />
+      <br />
+      We had a great time delving into the technical and game theoretic
+      underpinnings of decentralized arbitration{` `}
+      <span role="img" aria-label="scales">
+        ⚖️
+      </span>
+    </ContentTypography>
+  );
+};
+
+const NearText = () => {
+  return (
+    <ContentTypography variant="subtitle2">
+      Thanks{` `}
+      <PostLink>@NEARProtocol</PostLink>
+      {` `}
+      for having us tonight and allowing us to share more about dOrg and our
+      mission
+      <br />
+      <PostLink>@hgarciaalcala</PostLink>
+      {` `}
+      <PostLink>@nic_maq</PostLink>
+    </ContentTypography>
+  );
+};
+
+const MackenzieText = () => {
+  return (
+    <ContentTypography variant="subtitle2">
+      <span role="img" aria-label="speech balloon">
+        💬
+      </span>{" "}
+      &ldquo;I think people are looking for more sustainable and fulfilling
+      careers [...] DAOs, including dOrg, are attracting people of all ages,
+      backgrounds and locations.&rdquo;
+      <br />
+      <br />
+      Read on: <PostLink>https://bit.ly/3FcEKi3</PostLink>
+    </ContentTypography>
+  );
+};
 
 export const communityContent = {
-  magenta: <MagentaText/>,
-  linus: <LinusText/>,
-  phil_h: <PhilText/>,
-  newsletter: <NewsletterText/>,
-  giveth: <GivethText/>,
-}
+  magenta: <MagentaText />,
+  linus: <LinusText />,
+  newsletter: <NewsletterText />,
+  phil_h: <PhilText />,
+  unconference: <UnconferenceText />,
+  granCanaria: <GranCanariaText />,
+  giveth: <GivethText />,
+  ethBerlin: <EthBerlinText />,
+  metaFam: <MetaFamText />,
+  kleros: <KlerosText />,
+  near: <NearText />,
+  mackenzie: <MackenzieText />,
+};
