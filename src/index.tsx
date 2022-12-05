b2c5198c825123b28e1dfbaa6749620248d6e916
Colin Spence <udappia@gmail.com>
2022-12-05 09:23:53 -0700

    Resolve syntax errors

--- src/index.tsx
+++ src/index.tsx
@@ -1,27 +1,27 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import ReactGA from 'react-ga';
-import { ThemeProvider, CssBaseline } from '@mui/material';
-import { HelmetProvider } from 'react-helmet-async';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import ReactGA from "react-ga";
+import { ThemeProvider, CssBaseline } from "@mui/material";
+import { HelmetProvider } from "react-helmet-async";
 
-import { HashRouter } from 'react-router-dom';
-import theme from './theme';
+import { HashRouter } from "react-router-dom";
+import theme from "./theme";
 
-const GA_ID: string = process.env.GA_ID || '';
+const GA_ID: string = process.env.GA_ID || "";
 ReactGA.initialize(GA_ID);
 
 ReactDOM.render(
-   <React.StrictMode>
-      <HashRouter>
-         <ThemeProvider theme={theme}>
-            <HelmetProvider>
-               <CssBaseline />
-               <App />
-            </HelmetProvider>
-         </ThemeProvider>
-      </HashRouter>
-   </React.StrictMode>,
-   document.getElementById('root')
+  <React.StrictMode>
+    <HashRouter>
+      <ThemeProvider theme={theme}>
+        <HelmetProvider>
+          <CssBaseline />
+          <App />
+        </HelmetProvider>
+      </ThemeProvider>
+    </HashRouter>
+  </React.StrictMode>,
+  document.getElementById("root")
 );
