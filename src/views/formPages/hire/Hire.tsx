4a6ad0ba44c212b9409e8006d20f783c8bb0b31c
fabiolalombardim <lombardifabiola@hotmail.com>
2023-01-19 18:23:26 +0100

    fixed gtm & ga implementation

--- src/views/formPages/hire/Hire.tsx
+++ src/views/formPages/hire/Hire.tsx
@@ -1,3 +1,4 @@
+import ReactGA from "react-ga";
 import React, { useState } from "react";
 import { Controller, FieldErrors, useForm } from "react-hook-form";
 import { FormPage } from "../FormPage";
@@ -12,7 +13,7 @@ import { colors } from "../../../theme";
 import { hireSelectOptions } from "../../../constants/hire";
 import { HireFormInputs } from "../../../utils/network";
 import useCreateHireRecord from "../../../api/airTable/hooks/useCreateHireRecord";
-import ReactGA from "react-ga";
+import TagManager from 'react-gtm-module';
 
 export const HireView: React.FC = () => {
   const { loading, createRecord } = useCreateHireRecord();
@@ -30,14 +31,27 @@ export const HireView: React.FC = () => {
     console.log("errors", errors);
   }
 
+  console.log(ReactGA.ga())
+
   const onSubmit = async (data: HireFormInputs) => {
     const submittedData: HireFormInputs = { ...data };
     await createRecord(submittedData);
     setSubmitted(true);
-    
+
+    const args = {
+      dataLayer: {
+        event: "submitted_hire_form"
+        /* can pass more variables here if needed */
+      },
+      dataLayerName: "HirePageLayer"
+    };
+    TagManager.dataLayer(args);
+
+    ReactGA.pageview("/hire");
     ReactGA.event({
-      category: 'hire_form_submit',
-      action: 'submitted_hire_form'
+      category: "hire_form_submit",
+      action: "submitted_hire_form",
+      label: "submitted_hire_form",
     });
   };
 
@@ -48,7 +62,11 @@ export const HireView: React.FC = () => {
     >
       {submitted ? (
         <>
-          <Stack spacing={2} id="hire_us_thank_you" className="hire_us_thank_you">
+          <Stack
+            spacing={2}
+            id="hire_us_thank_you"
+            className="hire_us_thank_you"
+          >
             <Typography variant="h2">Thanks for your interest!</Typography>
             <Typography variant="body1">
               We&apos;ve just received your submission and we will get back to
